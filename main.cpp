0bcf2d6096d156735b3291454d7a997fef9cd77a
KisVasi@sulid.hu <kisvasi@sulid.hu>
2022-09-08 18:40:43 +0200

    PROBLEMS NEED FIXIN

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //Name
+    std::cout << '1-100 ertekek duplazasa' //"" and ;
+    for (int i = 0;) //for
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //for
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //values and ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //double and initial value
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
