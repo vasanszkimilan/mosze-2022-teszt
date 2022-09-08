c3460c4a125678c0f8a591dcda3a9efec5a7eda4
KisVasi@sulid.hu <kisvasi@sulid.hu>
2022-09-08 18:44:03 +0200

    Problems fixed bossman

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //Name
-    std::cout << '1-100 ertekek duplazasa' //"" and ;
-    for (int i = 0;) //for
+    int *b = new int[N_ELEMENTS]; //Name
+    std::cout << "1-100 ertekek duplazasa"; //"" and ;
+    for (int i = 1; i < N_ELEMENTS, i++) //for
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //for
+    for (int i = 0; i<N_ELEMENTS; i++) //for
     {
-        std::cout << "Ertek:" //values and ;
+        std::cout << "Ertek:" << b[i] <<endl;//values and ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //double and initial value
+    double atlag = 0; //double and initial value
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i] //;
+        atlag += b[i]; //;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
